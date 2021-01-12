9f4b809f877ad5d61ea1210e7a5e4db8d09a5fab
huluobugen <1821044807@qq.com>
2021-01-12 15:02:10 +0800

    飞机订票系统

---
+++ src/OrderPlaneTicketSystem/Test.java
@@ -0,0 +1,7 @@
+package OrderPlaneTicketSystem;
+
+public class Test{
+    public static void main(String args[]){
+
+    }
+}
--- src/test11/text.java
+++ src/test11/text.java
@@ -2,6 +2,7 @@ package test11;
 
 public class text {
     public static void main(String[] args) {
+
         System.out.println("hello");
     }
 }
